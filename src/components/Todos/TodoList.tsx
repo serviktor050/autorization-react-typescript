6254f469b857e569da6b31aa5aa6b0d06d3b1d75
serviktor050 <mr.s-gagulin@yandex.ru>
2021-10-19 16:27:02 +0300

    Добавлен функционал добавления выполнения задачи и удаления задачи

--- src/components/Todos/TodoList.tsx
+++ src/components/Todos/TodoList.tsx
@@ -3,20 +3,46 @@ import { ITodo } from "../../intefaces";
 
 type TodoListProps = {
   todos: ITodo[];
+  onToggle(id: number): void;
+  onRemove: (id: number) => void;
 };
 
-export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+export const TodoList: React.FC<TodoListProps> = ({
+  todos,
+  onToggle,
+  onRemove,
+}) => {
   if (todos.length === 0) {
     return <p className="center">Список задач пуст</p>;
   }
+
+  const removeHandler = (event: React.MouseEvent, id: number) => {
+    event.preventDefault();
+    onRemove(id);
+  };
+
   return (
     <ul>
       {todos.map((todo) => {
         const classes = ["todo"];
+        if (todo.completed) {
+          classes.push("completed");
+        }
         return (
           <li className={classes.join(" ")} key={todo.id}>
             <label>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => onToggle(todo.id)}
+              />
               <span>{todo.title}</span>
+              <i
+                className="material-icons red-text"
+                onClick={(event) => removeHandler(event, todo.id)}
+              >
+                delete
+              </i>
             </label>
           </li>
         );
--- src/components/pages/Todos/Todos.tsx
+++ src/components/pages/Todos/Todos.tsx
@@ -3,6 +3,8 @@ import { TodoForm } from "../../Todos/TodoForm";
 import { TodoList } from "../../Todos/TodoList";
 import { ITodo } from "../../../intefaces";
 
+declare var confirm: (question: string) => boolean;
+
 export const Todos: React.FC = () => {
   const userTokenLocalStorage = JSON.parse(
     JSON.stringify(localStorage.getItem("token"))
@@ -30,13 +32,38 @@ export const Todos: React.FC = () => {
     setTodos((prev) => [newTodo, ...prev]);
   };
 
+  const toggleHandler = (id: number) => {
+    setTodos((prev) =>
+      prev.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const removeHandler = (id: number) => {
+    const shouldRemove = confirm("Вы уверены, что хотите удалить элемент?");
+    if (shouldRemove) {
+      setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    }
+  };
+
   return (
     <div className="page-container">
       {userTokenLocalStorage && (
         <>
           <h1>Todos</h1>
           <TodoForm onAdd={addHandlerTodo} />
-          <TodoList todos={todos} />
+          <TodoList
+            todos={todos}
+            onToggle={toggleHandler}
+            onRemove={removeHandler}
+          />
         </>
       )}
 
